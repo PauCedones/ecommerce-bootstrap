86ddb923670f10f6b7db27c8f39d576c3c8d1ba0
Paula <cedones@gmail.com>
2020-05-11 01:06:57 -0300

    termine? termine

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
+
+//SPINNER LOADING
 $(document).ready(function() {
     $("#btnFetch").click(function() {
     // disable button
     $(this).prop("disabled", true);
     // add spinner to button
     $(this).html(
-    '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div> cargando...'
+    '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div> Cargando datos'
     );
     });
     });
+
